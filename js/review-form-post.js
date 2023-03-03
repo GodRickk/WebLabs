1ae61b0df59f17e8e5eaf4842915b199684e49b7
Ilya Antipin <morlok01@inbox.ru>
2023-03-03 13:35:08 +0300

    feat: add local storage support

--- js/review-form-post.js
+++ js/review-form-post.js
@@ -1,23 +1,46 @@
+var count = localStorage.length;
+var comments = {};
+
+
 let form = document.querySelector('form.review-form');
 let reviewsColumnContainer = document.querySelector('div.reviews-column__container');
 let textReviewInput = document.querySelector('#text-review');
 let nameInput = document.querySelector('#user-name');
 
-form.addEventListener('submit',(e) => {
-    e.preventDefault();
-    let textReview = textReviewInput.value;
-    let name = nameInput.value;
-    let template = `
-            <div class="reviews-text__container">
-                <div class="reviews-text__item">
-                    <div class="reviews-text__text">${textReview}</div>
-                    <div class="reviews-text__author">${name}</div>   
-                </div>
-            </div>`;
-    reviewsColumnContainer.innerHTML += template;
-    console.log()
-    closeForm();
-});
+// form.addEventListener('submit',(e) => {
+//     e.preventDefault();
+//     let textReview = textReviewInput.value;
+//     let name = nameInput.value;
+//     let template = `
+//             <div class="reviews-text__container">
+//                 <div class="reviews-text__item">
+//                     <div class="reviews-text__text">${textReview}</div>
+//                     <div class="reviews-text__author">${name}</div>   
+//                 </div>
+//             </div>`;
+//     reviewsColumnContainer.innerHTML += template;
+//     console.log()
+//     closeForm();
+// });
+
+
+// form.addEventListener('submit',(e) => {
+//     e.preventDefault();
+//     let textReview = textReviewInput.value;
+//     let name = nameInput.value;
+//     let template = `
+//             <div class="reviews-text__container">
+//                 <div class="reviews-text__item">
+//                     <div class="reviews-text__text">${textReview}</div>
+//                     <div class="reviews-text__author">${name}</div>   
+//                 </div>
+//             </div>`;
+//     reviewsColumnContainer.innerHTML += template;
+//     console.log()
+//     closeForm();
+// });
+
+
 
 function closeForm(){
     let reviewForm = document.querySelector('.review-form');
@@ -27,13 +50,164 @@ function closeForm(){
 }
 
 
+form.addEventListener('submit',(e) => {
+    let textReviewInput = document.querySelector('#text-review');
+    let nameInput = document.querySelector('#user-name');
 
-// var articleDiv = document.querySelector("div.article");
-// // создаем элемент
-// var elem = document.createElement("h2");
-// // создаем для него текст
-// var elemText = document.createTextNode("Привет мир");
-// // добавляем текст в элемент в качестве дочернего элемента
-// elem.appendChild(elemText);
-// // добавляем элемент в блок div
-// articleDiv.appendChild(elem);
+
+    // Создание html элементов со стилями
+    var reviewsTextContainer = document.createElement("div");
+    reviewsTextContainer.setAttribute('class', 'reviews-text__container');
+
+    var reviewsTextItem = document.createElement("div");
+    reviewsTextItem.setAttribute('class', 'reviews-text__item');
+
+    var reviewsTextText = document.createElement('div')
+    reviewsTextText.setAttribute('class', 'reviews-text__text');
+
+    var reviewsTextAuthor = document.createElement('div')
+    reviewsTextAuthor.setAttribute('class', 'reviews-text__author');
+
+    reviewsTextText.innerHTML = textReviewInput.value;
+    reviewsTextAuthor.innerHTML = nameInput.value;
+
+
+    // Создание DOM иерархии
+    reviewsColumnContainer.appendChild(reviewsTextContainer);
+    reviewsTextContainer.appendChild(reviewsTextItem);
+    reviewsTextItem.appendChild(reviewsTextText);
+    reviewsTextItem.appendChild(reviewsTextAuthor);
+
+
+    comments.text = textReviewInput.value;
+    count++; // 1
+    comments.textId = localStorage.length + 1; // 1
+    localStorage.setItem(count, comments.text);
+
+
+    comments.author = nameInput.value;
+    count++; // 2
+    comments.authorId = localStorage.length + 2; // 2
+    localStorage.setItem(count, comments.author);
+    closeForm();
+});
+
+function createComment() {
+    let textReviewInput = document.querySelector('#text-review');
+    let nameInput = document.querySelector('#user-name');
+
+    // comments.text = textReviewInput.value;
+    // comments.author = nameInput.value;
+
+    // count++; // 1
+    // comments.textId = localStorage.length + 1; // 1
+    // count++; // 2
+    // comments.authorId = localStorage.length + 2; // 2
+
+
+    // Создание html элементов со стилями
+    var reviewsTextContainer = document.createElement("div");
+    reviewsTextContainer.setAttribute('class', 'reviews-text__container');
+
+    var reviewsTextItem = document.createElement("div");
+    reviewsTextItem.setAttribute('class', 'reviews-text__item');
+
+    var reviewsTextText = document.createElement('div')
+    reviewsTextText.setAttribute('class', 'reviews-text__text');
+
+    var reviewsTextAuthor = document.createElement('div')
+    reviewsTextAuthor.setAttribute('class', 'reviews-text__author');
+
+
+    // Текст комментраия и автор
+    // reviewsTextText.innerHTML = comments.text;
+    // reviewsTextAuthor.innerHTML = comments.author;
+
+    reviewsTextText.innerHTML = textReviewInput.value;
+    reviewsTextAuthor.innerHTML = nameInput.value;
+
+
+    // Создание DOM иерархии
+    reviewsColumnContainer.appendChild(reviewsTextContainer);
+    reviewsTextContainer.appendChild(reviewsTextItem);
+    reviewsTextItem.appendChild(reviewsTextText);
+    reviewsTextItem.appendChild(reviewsTextAuthor);
+
+
+    comments.text = textReviewInput.value;
+    count++; // 1
+    comments.textId = localStorage.length + 1; // 1
+    localStorage.setItem(count, comments.text);
+
+
+    comments.author = nameInput.value;
+    count++; // 2
+    comments.authorId = localStorage.length + 2; // 2
+    localStorage.setItem(count, comments.author);
+}
+
+
+function showReviews() {
+
+    for (var i = 0; i < localStorage.length; i += 2){
+        var dynamicCommentBox = document.createElement('div');
+        dynamicCommentBox.style.display = "block";
+
+        var reviewsTextContainer = document.createElement("div");
+        reviewsTextContainer.setAttribute('class', 'reviews-text__container');
+
+        var reviewsTextItem = document.createElement("div");
+        reviewsTextItem.setAttribute('class', 'reviews-text__item');
+
+        var reviewsTextText = document.createElement('div')
+        reviewsTextText.setAttribute('class', 'reviews-text__text');
+
+        var reviewsTextAuthor = document.createElement('div')
+        reviewsTextAuthor.setAttribute('class', 'reviews-text__author');
+
+        reviewsTextText.innerHTML = localStorage.getItem(i + 1);
+        reviewsTextAuthor.innerHTML = localStorage.getItem(i + 2);
+
+        reviewsColumnContainer.appendChild(reviewsTextContainer);
+        reviewsTextContainer.appendChild(reviewsTextItem);
+        reviewsTextItem.appendChild(reviewsTextText);
+        reviewsTextItem.appendChild(reviewsTextAuthor);
+    }
+}
+
+
+function pageLoad() {
+    showReviews();
+}
+
+window.addEventListener('load', pageLoad);
+
+
+
+
+// заготовка
+// добавить все в евент листнер и посмотреть как будет работать
+// сохранение извлечение из локального хранилища
+
+// var reviewsTextContainer = document.createElement("div");
+// reviewsTextContainer.setAttribute('class', 'reviews-text__container');
+
+// var reviewsTextItem = document.createElement("div");
+// reviewsTextItem.setAttribute('class', 'reviews-text__item');
+
+// var reviewsTextText = document.createElement('div')
+// reviewsTextText.setAttribute('class', 'reviews-text__text');
+
+// var reviewsTextAuthor = document.createElement('div')
+// reviewsTextAuthor.setAttribute('class', 'reviews-text__author');
+
+// let textReview = textReviewInput.value;
+// let authorName = nameInput.value;
+
+// reviewsTextText.innerHTML = textReview;
+// reviewsTextAuthor.innerHTML = authorName;
+ 
+// reviewsTextContainer.appendChild(reviewsTextItem);
+// reviewsTextItem.appendChild(reviewsTextText);
+// reviewsTextItem.appendChild(reviewsTextAuthor);
+// reviewsColumnContainer.appendChild(reviewsTextContainer);
